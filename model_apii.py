11bbeea879b22c931b30db1342bf1fbac49bd3ad
Nazeeya Azeem <57052959+naz2001@users.noreply.github.com>
2023-05-12 22:42:14 +0530

    Update model_apii.py

--- model_apii.py
+++ model_apii.py
@@ -17,6 +17,7 @@ comfort_prediction = pickle.load(open('comfort_prediction.sav','rb'))
 
 @app.post('/comfort_prediction')
 def comfort_pred(input_parameters: model_input):
+    return {"message": "Hello, World!1"}
     age = input_parameters.Age
     gender = input_parameters.Gender
     ca = input_parameters.CurrentActivity
@@ -24,10 +25,10 @@ def comfort_pred(input_parameters: model_input):
     t = input_parameters.Temperature
     h = input_parameters.Humidity
     w = input_parameters.WindSpeed
-
+    return {"message": "Hello, World!2"}
     input_list = [age, gender, ca, cl, t, h, w]
     prediction = comfort_prediction.predict([input_list])
-
+    return {"message": "Hello, World!3"}
     if prediction[0] == 10:
         return 'Comfortable'
     elif prediction[0] == 20:
